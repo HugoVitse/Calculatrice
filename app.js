e01c4917885f4f00b20588f9abb27c2f26e163e7
HugoVitse <90216651+HugoVitse@users.noreply.github.com>
2023-03-27 19:33:14 +0200

    push etape 3/4

--- app.js
+++ app.js
@@ -1,41 +1,77 @@
-var screen = document.getElementById("screen")
-var size = 0
-var tab = []
-var nombreparent = 0
-var nombreparferm = 0
-var error = false
-var newcalc = false
-var compteur=0
-var op = ["+","-","×","÷"]
-function button(id){
-    if(id !="C" && id!="="){
-        if(newcalc == true){
-            screen.innerText="";
-            newcalc=false;
-        }
-        screen.innerText+=id;
-        console.log(id)
-        if(op.includes(id)==true){
-            console.log(screen.innerText[screen.innerText.length -2])
-            if(op.includes(screen.innerText[screen.innerText.length -2])) error = true;
-        }
-        if(id == "(") nombreparent++
-        if(id==")") nombreparferm++
-    } 
-    else{
-        if(id == "C") screen.innerText = "";
-        
-        if(id == "="){
-            if(error == true || (nombreparent != nombreparferm) ){
-                screen.innerText = "Erreur"
-                newcalc = true;
-                error = false;
+class BaseCalculator {
+    constructor() {
+        this.id = undefined
+        this.screen = document.getElementById("screen")
+        this.error = false
+        this.nombreparent = 0
+        this.nombreparferm = 0
+        this.newcalc = false
+        this.op = ["+","-","×","÷"]
+        this.operande = undefined
+        this.resultat = undefined
+    }
+    retourArriere(){
+        this.screen.innerText = this.screen.innerText.substring(0,this.screen.innerText.length-1)
+    }
+    affichage(ide){
+        this.id = ide
+        if(this.id !="C" && this.id!="="){
+            if(this.newcalc == true){
+                this.effacer();
+                this.newcalc=false;
             }
-            else{
-                var final = screen.innerText.replace("×","*")
-                var final2 = final.replace("÷","/")
-                screen.innerText= (eval(final2))
+            this.screen.innerText+=this.id;
+            if(this.op.includes(this.id)==true){
+                if(this.op.includes(this.screen.innerText[this.screen.innerText.length -2])) this.error = true;
+            }
+            if(this.id == "(") this.nombreparent++
+            if(this.id==")") this.nombreparferm++
+        } 
+        else{
+            if(this.id == "C") this.effacer();
+            
+            if(this.id == "="){
+                if(this.error == true || (this.nombreparent != this.nombreparferm) ){
+                    console.log(this.error)
+                    this.screen.innerText = "Erreur"
+                    this.newcalc = true;
+                    this.error = false;
+                }
+                else{
+                    var final = this.screen.innerText.replace("×","*")
+                    var final2 = final.replace("÷","/")
+                    this.operande = final2
+                    this.operation()
+                    this.screen.innerText = (this.resultat)
+                }
             }
         }
     }
+    operation(){
+        this.resultat = eval(this.operande)
+    }
+    effacer(){
+        this.screen.innerText = ""
+    }
+
+    
 }
+let baseCalculator = new BaseCalculator();
+
+document.addEventListener('keydown', (event) => { // possibilité d'intéragir avec le clavier
+    console.log(event.key)
+    if( (parseInt(event.key) >= 0 && parseInt(event.key) <= 9) || (event.key == "/" || event.key == "*" || event.key == "(" || event.key == ")" || event.key == "+" || event.key == "-" || event.key == "." || event.key == "C" )){
+        baseCalculator.affichage(event.key)
+    }
+    if(event.key == "," ){
+        baseCalculator.affichage(".")
+    }
+    if(event.key == "Enter"){
+        baseCalculator.affichage("=")
+    }
+    if(event.key == "Backspace"){ // bouton de retour arrière
+        baseCalculator.retourArriere()
+
+    }
+    // do something
+});
